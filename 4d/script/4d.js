52559c3ff9ea5de2f7232e9fc274141751da0a13
ery <ery.gibi.pro@gmail.com>
2025-03-04 13:47:02 +0800

    need to defer in script unless dom manipulation fail

--- 4d/script/4d.js
+++ 4d/script/4d.js
@@ -4,3 +4,43 @@ console.log(a)
 var b = Math.random()* 10000
 b = Math.floor(b)
 console.log(b)
+
+create_p()
+
+function create_4d()
+{
+    //fucntion to generate random 4d number
+    var n = Math.random()*10000
+    n = Math.floor(n)
+    if(n > 1000)
+    {
+    
+    }
+    else if( n > 100)
+    {
+        n = String(n)
+        n = '0'+n
+    }
+    else if ( n > 10)
+    {
+        n = String(n)
+        n = '00'+n
+    }
+    else
+    {
+        n = String(n)
+        n = "000" + n
+    }
+    return n
+}
+
+function create_p()
+{
+    let p = document.createElement("p")
+    p.textContent = "hello world"
+   
+    console.log(p)
+    var a = document.getElementById('di')
+    a.appendChild(p)
+
+}
